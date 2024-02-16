2fc315a9fa60c341d4082983a3734333a38b5aab
Oscar Ruiz <oscarruiz2614@gmail.com>
2024-02-15 22:28:33 -0500

    feat: :ambulance: seeder users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'nombres' => 'Oscar',
+                'apellidos' => 'Ruiz',
+                'email' => 'oscarruiz2614@gmail.com',
+                'password' => Hash::make('12345')
+            ],
+        ];
+
+        foreach ($users as $key => $user) {
+            User::create($user);
+        }
+    }
+}
